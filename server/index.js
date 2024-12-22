866e1a9bab4cb9b9ae1b555f45748c8569cbb050
vostsoldier <fantasyindeath86@gmail.com>
2024-12-21 22:18:59 -0500

    railway is going wrong

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(cors({
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
-    return callback(null, true);
+    return callback(null, true); 
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
